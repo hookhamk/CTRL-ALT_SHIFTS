ad2e52e59ec6ba4ac01e4dd68bb3ee7e8c60cb10
Kelly Hookham <hookhamk8@gmail.com>
2025-03-18 18:40:09 -0500

    tables

--- client/src/pages/Employer/Employees.tsx
+++ client/src/pages/Employer/Employees.tsx
@@ -1,15 +1,151 @@
-import Table from "../../components/ui/table";
 
-function Employees() {
-    return (
-      <div>
-        <h1>View Employees</h1>
-        <p>This page is a table to hold employees</p>
-        <div>
-            <Table />
+interface Person {
+  name: string;
+  title: string;
+  email: string;
+  role: string;
+}
+
+const user = JSON.parse(localStorage.getItem('user') || '{}');
+const employee_name = user.first_name;
+
+const people: Person[] = [
+  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+  // More people...
+];
+
+function classNames(...classes: (string | boolean)[]): string {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Employees() {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold text-gray-900">Welcome {employee_name}!</h1>
+          <p className="mt-2 text-sm text-gray-700">
+          Please see your weekly schedule below.
+          </p>
+        </div>
+        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+          <button
+            type="button"
+            className="block rounded-md bg-slate-500 px-3 py-2 text-center text-sm font-semibold text-white shadow-xs hover:bg-lime-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600"
+          >
+           Look up another date
+          </button>
         </div>
       </div>
-    );
-  }
-  
-  export default Employees;
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle">
+            <table className="min-w-full border-separate border-spacing-0">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter sm:pl-6 lg:pl-8"
+                  >
+                    Job Assignment
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 hidden border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter sm:table-cell"
+                  >
+                    Monday
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 hidden border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter lg:table-cell"
+                  >
+                    Tuesday
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter"
+                  >
+                    Wednesday
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter"
+                  >
+                    Thursday
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter"
+                  >
+                    Friday
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter"
+                  >
+                    Saturday
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter"
+                  >
+                    Sunday
+                  </th>
+                </tr>
+              </thead>
+              {/* mutations go here. Reformat for weekly schdule by employee */}
+              <tbody>
+                {people.map((person, personIdx) => (
+                  <tr key={person.email}>
+                    <td
+                      className={classNames(
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
+                        'py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-6 lg:pl-8',
+                      )}
+                    >
+                      {person.name}
+                    </td>
+                    <td
+                      className={classNames(
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
+                        'hidden px-3 py-4 text-sm whitespace-nowrap text-gray-500 sm:table-cell',
+                      )}
+                    >
+                      {person.title}
+                    </td>
+                    <td
+                      className={classNames(
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
+                        'hidden px-3 py-4 text-sm whitespace-nowrap text-gray-500 lg:table-cell',
+                      )}
+                    >
+                      {person.email}
+                    </td>
+                    <td
+                      className={classNames(
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
+                        'px-3 py-4 text-sm whitespace-nowrap text-gray-500',
+                      )}
+                    >
+                      {person.role}
+                    </td>
+                    <td
+                      className={classNames(
+                        personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
+                        'relative py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-8 lg:pr-8',
+                      )}
+                    >
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Edit<span className="sr-only">, {person.name}</span>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- client/src/pages/Employer/Jobs.tsx
+++ client/src/pages/Employer/Jobs.tsx
@@ -1,15 +1,99 @@
-import Table from "../../components/ui/table";
 
-function Jobs() {
-    return (
-      <div>
-        <h1>View Jobs</h1>
-        <p>This page is a table to hold jobs</p>
-        <div>
-            <Table />
+interface Job {
+  id: number;
+  job_title: string;
+  employee_count: number;
+}
+
+const job: Job[] = [
+  { id: 1, job_title: 'Front-end Developer', employee_count: 4},
+  // More people...
+];
+
+function classNames(...classes: (string | boolean)[]): string {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Jobs() {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold text-gray-900">View Jobs</h1>
+          <p className="mt-2 text-sm text-gray-700">
+          Please current jobs in your company.
+          </p>
+        </div>
+        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+          <button
+            type="button"
+            className="block rounded-md bg-slate-500 px-3 py-2 text-center text-sm font-semibold text-white shadow-xs hover:bg-lime-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600"
+          >
+          Create Job
+          </button>
         </div>
       </div>
-    );
-  }
-  
-  export default Jobs;
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle">
+            <table className="min-w-full border-separate border-spacing-0">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 border-b border-gray-300 bg-white/75 py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter sm:pl-6 lg:pl-8"
+                  >
+                    Job Id
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 hidden border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter sm:table-cell"
+                  >
+                    Job Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="sticky top-0 z-10 hidden border-b border-gray-300 bg-white/75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur-sm backdrop-filter lg:table-cell"
+                  >
+                    Employee Count
+                  </th>
+                </tr>
+              </thead>
+              {/* mutations go here. Reformat for weekly schdule by employee */}
+              <tbody>
+                {job.map((job, jobIdx) => (
+                  <tr key={job.id}>
+                    <td
+                      className={classNames(
+                        jobIdx !== job.length - 1 ? 'border-b border-gray-200' : '',
+                        'py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-6 lg:pl-8',
+                      )}
+                    >
+                      {job.id}
+                    </td>
+                    <td
+                      className={classNames(
+                        jobIdx !== job.length - 1 ? 'border-b border-gray-200' : '',
+                        'hidden px-3 py-4 text-sm whitespace-nowrap text-gray-500 sm:table-cell',
+                      )}
+                    >
+                      {job.job_title}
+                    </td>
+                    <td
+                      className={classNames(
+                        jobIdx !== job.length - 1 ? 'border-b border-gray-200' : '',
+                        'hidden px-3 py-4 text-sm whitespace-nowrap text-gray-500 lg:table-cell',
+                      )}
+                    >
+                      {job.employee_count}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
