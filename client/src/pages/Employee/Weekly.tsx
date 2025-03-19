b31e8e384d6591cf5c9eb2b368c2aaf44a358325
Tristan Persaud <tristanppersaud@gmail.com>
2025-03-19 12:52:43 -0500

    calendar-fix

--- client/src/pages/Employee/Weekly.tsx
+++ client/src/pages/Employee/Weekly.tsx
@@ -14,16 +14,16 @@ interface Schedule {
   end_time: string;
 }
 
-// Days order mapping
-const daysOrder: Record<string, number> = {
-  Monday: 1,
-  Tuesday: 2,
-  Wednesday: 3,
-  Thursday: 4,
-  Friday: 5,
-  Saturday: 6,
-  Sunday: 0,
-};
+// // Days order mapping
+// const daysOrder: Record<string, number> = {
+//   Monday: 1,
+//   Tuesday: 2,
+//   Wednesday: 3,
+//   Thursday: 4,
+//   Friday: 5,
+//   Saturday: 6,
+//   Sunday: 0,
+// };
 
 function Weekly() {
   
